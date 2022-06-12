cd60ef4fe82083affc099f876bc9922cf7452fcb
Tharit <tharit@bonmek.com>
2022-06-12 17:49:23 +0700

    fix resolve

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -25,11 +25,7 @@ export class HeroDetailComponent implements OnInit {
   ngOnInit() {
     console.log("Awdawdaw")
     const id = this.route.snapshot.paramMap.get('id');
-<<<<<<< HEAD
-    console.log("test 111")
-=======
     console.log("test 777")
->>>>>>> origin/master
     this.heroService.getHero(id).subscribe(snapshot => {
       this.hero = {
         id: snapshot.payload.id,
