c2bc769537a59f05b766db00d3c3784f977c546d
Tharit <tharit@bonmek.com>
2022-08-05 22:05:31 +0700

    clean console

--- src/app/hero/hero.service.ts
+++ src/app/hero/hero.service.ts
@@ -11,28 +11,28 @@ export class HeroService {
   ) { }
 
   listHeroes(filter?: QueryFn) {
-    console.log("gggg")
+    
     return this.db.collection('heroe', filter).snapshotChanges()
   }
 
   getHero(id: string) {
     // let doc = this.db.doc(path)
-    console.log("gggg")
+    
     return this.db.doc(`heroe/${id}`).snapshotChanges()
   }
 
   addHero(hero) {
-    console.log("gggg")
+    
     return this.db.collection('heroe').add(hero)
   }
 
   deleteHero(id) {
-    console.log("gggg")
+    
     return this.db.doc(`heroe/${id}`).delete()
   }
 
   editHero(id, hero) {
-    console.log("gggg")
+    
     return this.db.doc(`heroe/${id}`).update(hero)
   }
 
